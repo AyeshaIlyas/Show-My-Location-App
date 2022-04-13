dfa9385d2a7e7ec4b04b331820ac06768ae37266
Ayesha Ilyas <annie16950@gmail.com>
2022-04-13 09:40:24 -0400

    Initial Commit

---
+++ ShowMyLocationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShowMyLocationApp
+//
+//  Created by Ayesha Ilyas on 4/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShowMyLocationApp/ShowMyLocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMyLocationAppApp.swift
+//  ShowMyLocationApp
+//
+//  Created by Ayesha Ilyas on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ShowMyLocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
